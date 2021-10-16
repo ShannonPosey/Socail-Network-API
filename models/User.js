27a984a8d4b1132dbda0ff88fb15450ac6280862
shannon_p <snp1284kasey@aol.com>
2021-10-15 20:02:20 -0400

    added schema to the user model

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const {Schema, model} = require("mongoose");
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            trim: true,
+            // unique:
+        },
+        email: {
+            type: String,
+            required: true,
+            // unique:
+            
+        }
+    }
+);
